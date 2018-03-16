6b2a6b16167e10852822b3661669759bce1adb92
Olli <oltsuwh@gmail.com>
2018-03-16 14:31:09 +0200

    T4 ESlint for frontend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ExampleComponent from './components/ExampleComponent';
 
 class App extends React.Component {
