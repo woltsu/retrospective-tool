2b9615647c415b5808883bf7fa0f12fb7d6722f1
unknown <jeremi.nieminen@gmail.com>
2018-03-18 15:48:53 +0200

    removed line-endings rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,10 +23,6 @@ module.exports = {
             "error",
             2
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
         "semi": [
             "error",
             "always"
